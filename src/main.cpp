34b5c8e7bfbbbcde991b56cf0e693d728034ce20
Stephane Bres <sbres@e3r10p18.42.fr>
2015-01-18 17:37:08 +0100

    Main menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sbres <sbres@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/17 12:17:47 by jbernabe          #+#    #+#             */
-/*   Updated: 2015/01/18 17:10:32 by sbres            ###   ########.fr       */
+/*   Updated: 2015/01/18 17:36:21 by sbres            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include "../inc/TerminalDisplay.class.hpp"
 #include "../inc/GraphicDisplay.class.hpp"
 #include "../inc/IMonitorModule.interface.hpp"
+#include "../inc/IMonitorDisplay.interface.hpp"
 #include "../inc/ramModule.class.hpp"
 #include <list>
 #include <unistd.h>
@@ -29,10 +30,29 @@ int main(){
 	/*std::cout << "coucouc" << std::endl;*/
 	//Get_clock *tata = new Get_clock();
 
-	Gkrellm *gk = new Gkrellm();
-	bool		runnig = true;
-	IMonitorDisplay *inter = new TerminalDisplay(gk->getName(), gk->getNodeName(), gk->getSysName(), gk->getNbCore());
-	//IMonitorDisplay *inter = new GraphicDisplay();
+	Gkrellm *gk = new		Gkrellm();
+	bool		runnig =	true;
+	std::string				input;
+	IMonitorDisplay *inter;
+
+	std::cout << "Tapez G pour mode graphique et T pour mode terminal." << std::endl;
+	std::cin >> input;
+	if (input.compare("G") == 0)
+	{
+		std::cout << "Mode Graphique !" << std::endl;
+		inter = new GraphicDisplay();
+	}
+	else if(input.compare("T") == 0)
+	{
+		std::cout << "Mode Terminal !" << std::endl;
+		inter = new TerminalDisplay(gk->getName(), gk->getNodeName(), gk->getSysName(), gk->getNbCore());
+	}
+	else
+	{
+		std::cout << "Pas de mode choisi !" << std::endl;
+		return 1;
+	}
+
 	IMonitorModule *ram = new RamModule();
 	IMonitorModule *ramu = new RamModule("RAM Used");
 	inter->init();
